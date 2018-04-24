9a5d3391da59d3bc5ee77eaf218fdbab47468671
L1lith <lily@lillith.pw>
2018-04-24 16:40:27 -0600

    fix syntax error

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import mutationWatcher from '@f/mutationWatcher'
+import mutationWatcher from './functions/mutationWatcher'
+import setupExistingStatbars from './functions/setupExistingStatbars'
 
-(new MutationObserver(mutationWatcher)).observe(document.body, {
-  childList: true,
-  subtree: true
+function run() {
+  (new MutationObserver(mutationWatcher)).observe(document.body, {
+    childList: true,
+    subtree: true
+  })
+  setupExistingStatbars()
+}
+
+window.addEventListener('load', ()=>{
+  const script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.textContent = content
+  let hasRun = false
+  script.onload = () => {hasRun = hasRun || run() || true} // Protects from being called multiple times
+  document.getElementsByTagName('head')[0].appendChild(script)
 })
-setupExistingStatbars()
-
-})()
