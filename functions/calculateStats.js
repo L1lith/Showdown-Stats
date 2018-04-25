2f6a65a24a333adea511e1ca5c651af66df8d3cd
L1lith <lily@lillith.pw>
2018-04-24 18:19:11 -0600

    fix issue with not passing nature factor to getNatureFactor

--- functions/calculateStats.js
+++ functions/calculateStats.js
@@ -6,14 +6,14 @@ function calculateStats(pokemon) {
   const output = {}
   Object.keys(pokemon.baseStats).forEach(stat => {
     const calc = statCalculator[stat === 'hp' ? 'calHpStats' : 'calUnhpStats'].bind(statCalculator)
-    const natureFactor = stat !== 'hp' ? getNatureFactor(pokemon) : undefined
+    const natureFactor = stat !== 'hp' ? getNatureFactor(pokemon, stat) : undefined
     const low = calc(31, [pokemon.baseStats[stat]], [0], pokemon.level, pokemon.nature || undefined)
     const high = calc(31, [pokemon.baseStats[stat]], [252], pokemon.level, pokemon.nature || undefined)
     let modifier = stat !== 'hp' && pokemon.boosts.hasOwnProperty(stat) ? statStageToDecimal(pokemon.boosts[stat]) : 1
     if (stat === 'spd' && pokemon.status === 'par') modifier *= window.room.battle.gen >= 7 ? 0.5 : 0.75
     const lowFinal = low * modifier
     const highFinal = high * modifier
-    output[stat] = {low, high, modifier, lowFinal, highFinal}
+    output[stat] = {low, high, modifier, lowFinal, highFinal, natureFactor: natureFactor || 1}
   })
   return output
 }
