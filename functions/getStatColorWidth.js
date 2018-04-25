b238ee2d8a10d96574438e0f5b5b7ff887399e69
L1lith <lily@lillith.pw>
2018-04-24 23:04:08 -0600

    create function for converting stat to color and width

---
+++ functions/getStatColorWidth.js
@@ -0,0 +1,13 @@
+import hslToHex from './hslToHex'
+
+function getStatColorWidth(name, value) {
+  let width = stats[j] * 75 / 504
+	if (name === 'hp') width = value * 75 / 704
+	if (width > 75) width = 75
+  let color = Math.floor(value * 180 / 714)
+	if (color > 360) color = 360
+  color = hslToHex(color,40,75)
+  return {color, width}
+}
+
+export default getStatColorWidth
