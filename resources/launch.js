1696b2ec06c8601b96579f7195742e4f344ba795
L1lith <lily@lillith.pw>
2018-05-03 19:44:01 -0600

    add custom font

--- resources/launch.js
+++ resources/launch.js
@@ -2,5 +2,9 @@ window.addEventListener('load', ()=>{
   const script = document.createElement('script')
   script.setAttribute('type', 'text/javascript')
   script.setAttribute('src', chrome.extension.getURL('/build.js'))
-  document.body.appendChild(script)
+  document.head.appendChild(script)
+  const font = document.createElement('link')
+  font.href = "https://fonts.googleapis.com/css?family=Open+Sans"
+  font.rel = "stylesheet"
+  document.head.appendChild(font)
 })
