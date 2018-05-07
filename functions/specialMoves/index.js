29999a01622290d04bb7feb02a920f790542ffde
L1lith <lily@lillith.pw>
2018-05-07 13:26:19 -0600

    improve special moves index and create return

--- functions/specialMoves/index.js
+++ functions/specialMoves/index.js
@@ -3,7 +3,19 @@ const output = {}
 const specialMoves = require.context(__dirname, false, /^(?!.*index).*\.js$/)
 
 specialMoves.keys().forEach(filePath => {
-  output[path.basename(filePath, path.extname(filePath))] = specialMoves(filePath)
+  let required = specialMoves(filePath)
+  if (required.hasOwnProperty('default')) required = required.default
+  if (!required.hasOwnProperty('name')) required.name = path.basename(filePath, path.extname(filePath))
+  if (typeof required.calculate == 'function') {
+    const oldCalculate = required.calculate
+    required.calculate = (pokemon, opponent, room, ...args) => {
+      if (typeof pokemon != 'object' || pokemon === null) throw new Error('Invalid Pokemon Input')
+      if (typeof opponent != 'object' || opponent === null) throw new Error('Invalid Opponent Pokemon Input')
+      if (typeof room != 'object' || room === null) throw new Error('Invalid Room Input')
+      return oldCalculate(pokemon, opponent, room, ...args)
+    }
+  }
+  output[required.name] = required
 })
 
-module.exports = output
+module.exports = Object.assign({}, output, {__esModule: true, default: output})
---
+++ functions/specialMoves/return.js
@@ -0,0 +1,5 @@
+function returnMove(pokemon, opponent, room) {
+  
+}
+
+export default {kind: 'baseDamage', calculate: returnMove}
