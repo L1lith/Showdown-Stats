b39765e3629f1eeae87c19c09e365fcd386bdc81
L1lith <lily@lillith.pw>
2018-05-11 19:47:33 -0600

    add beat up

---
+++ functions/specialMoves/beatUp.js
@@ -0,0 +1,6 @@
+function beatUp(pokemon, opponent, room) {
+  const attackTimes = room.battle.sides[0].pokemon.filter(pokemon => typeof pokemon.status == 'string' && pokemon.status.length > 0).length
+  return ((pokemon.baseStats.atk / 10) + 5) * attackTimes
+}
+
+export default {name: 'Beat Up', base: beatUp}
